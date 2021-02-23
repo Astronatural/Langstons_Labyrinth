34557fd68b7ec09a52a1a866d1dca72a197f19ce
JDK <jdkranzler@yahoo.com>
2021-02-22 21:28:53 -0600

    more framing

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,20 +1,49 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
-  // turn this into the GM screen page.
+  // AKA the GM screen page.
+  // const [newGame, setGame] = useState({ name:''});
+
+  // const loader() =>
+
+  // const deleter() => 
+
+  // const newGame() =>
+
   const user = useSelector((store) => store.user);
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <p>This is where you see your games and can create a new one!</p>
-      <LogOutButton className="btn" />
-    </div>
+    <>
+      <div className="container">
+        <h2>Welcome, {user.username}!</h2>
+        <p>Your ID is: {user.id}</p>
+        <p>This is where you load your Labyrinths from!</p>
+        {/* map over games linked to user_id here 
+      {game.map(game => {
+                    return (
+                        <div key={game.id} onClick={()=>loader(game)} >
+                            <h3>{game.name}</h3><h3>{game.totaltiles}</h3>
+                            <h3>{game.timestamp?}</h3><button onClick={deleter}>Delete Labyrinth</button>
+                        </div>
+                        <br>
+                    );
+                })}*/}
+        <p>This is where you make a new Labyrinth!</p>
+        <input
+          // value={newGame.name}
+          // onChange={(e) => setGame({ ...newGame, name: e.target.value })}
+          type="text"
+          id="name"
+          placeholder="Labyrinth Name" />
+        {/* <input>Set Labyrinth Width</input> */}
+        <button>Create and Load New Labyrinth</button>
+
+        <LogOutButton className="btn" />
+      </div>
+    </>
   );
 }
 
-// this allows us to use <App /> in index.js
 export default UserPage;
